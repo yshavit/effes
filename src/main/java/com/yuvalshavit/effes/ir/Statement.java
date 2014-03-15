8d05025a653dae6c292e4aa803a2785de832ae71
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-15 12:24:12 -0600

    generalize the compiler dispatch
    StatementCompiler will work similarly to ExpressionCompiler, with
    the dispatch and all. It'll also want the same test for dispatch.
    So, use some mild hackery to generalize that test.

---
+++ src/main/java/com/yuvalshavit/effes/ir/Statement.java
@@ -0,0 +1,14 @@
+package com.yuvalshavit.effes.ir;
+
+import com.google.common.collect.ImmutableList;
+
+public abstract class Statement implements EfNode {
+
+  private Statement() {}
+
+//  public static class IfElseStatement extends Statement {
+//    public IfElseStatement(Expression) {
+//
+//    }
+//  }
+}
--- src/test/java/com/yuvalshavit/effes/compile/expr/DispatchTest.java
+++ src/test/java/com/yuvalshavit/effes/compile/expr/DispatchTest.java
@@ -2,9 +2,13 @@ package com.yuvalshavit.effes.compile.expr;
 
 import com.google.common.collect.Sets;
 import com.yuvalshavit.effes.parser.EffesParser;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,10 +16,27 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class DispatchTest {
 
+  @DataProvider(name = "classes")
+  public static Object[][] classes() {
+    return new Object[][] {
+      new Object[] { ExpressionCompiler.Dispatch.class, EffesParser.ExprContext.class },
+      new Object[] {StatementCompiler.Dispatch.class, EffesParser.StatContext.class }
+    };
+  }
+
+  private final Class<?> dispatcherClass;
+  private final Class<?> contextBase;
+
+  @Factory(dataProvider = "classes")
+  public DispatchTest(Class<?> dispatcherClass, Class<?> contextBase) {
+    this.dispatcherClass = dispatcherClass;
+    this.contextBase = contextBase;
+  }
+
   @Test
   public void methods() {
     Set<Class<?>> exprClasses = findExprClasses();
-    Set<Class<?>> dispatchedClasses = Arrays.asList(ExpressionCompiler.Dispatch.class.getDeclaredMethods())
+    Set<Class<?>> dispatchedClasses = Arrays.asList(dispatcherClass.getDeclaredMethods())
       .stream()
       .filter(m -> m.getParameterTypes().length == 1)
       .map(m -> m.getParameterTypes()[0])
@@ -27,15 +48,30 @@ public class DispatchTest {
   @Test
   public void dispatchMap() {
     Set<Class<?>> exprClasses = findExprClasses();
-    Set<Class<?>> dispatchKeys = ExpressionCompiler.Dispatch.dispatchMap.keySet();
+    Set<Class<?>> dispatchKeys = getDispatchKeys();// ExpressionCompiler.Dispatch.dispatchMap.keySet();
     Set<Class<?>> diff = Sets.difference(exprClasses, dispatchKeys);
-    assertEquals("diff", Sets.<Class<?>>newHashSet(EffesParser.ExprContext.class), diff);
+    assertEquals("diff", Sets.<Class<?>>newHashSet(contextBase), diff);
+  }
+
+  private Set<Class<?>> getDispatchKeys() {
+    try {
+      Field dispatchMap = dispatcherClass.getField("dispatchMap");
+      Map<?, ?> m = (Map<?, ?>) dispatchMap.get(null);
+      Set<Class<?>> keys = Sets.newHashSet();
+      for (Object o : m.keySet()) {
+        Class<?> c = (Class<?>) o;
+        keys.add(c);
+      }
+      return keys;
+    } catch (Exception e) {
+      throw new AssertionError(e);
+    }
   }
 
   private Set<Class<?>> findExprClasses() {
     return Arrays.asList(EffesParser.class.getDeclaredClasses())
       .stream()
-      .filter(EffesParser.ExprContext.class::isAssignableFrom)
+      .filter(contextBase::isAssignableFrom)
       .collect(Collectors.toSet());
   }
 }
