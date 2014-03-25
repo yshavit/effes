216dfbd26c19b233156fb2013b75da10c0e34205
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-25 11:38:18 -0600

    make exception public

--- src/main/java/com/yuvalshavit/effes/compile/MethodRegistrationException.java
+++ src/main/java/com/yuvalshavit/effes/compile/MethodRegistrationException.java
@@ -1,6 +1,6 @@
 package com.yuvalshavit.effes.compile;
 
-class MethodRegistrationException extends RuntimeException {
+public class MethodRegistrationException extends RuntimeException {
   public MethodRegistrationException(String msg) {
     super(msg);
   }
