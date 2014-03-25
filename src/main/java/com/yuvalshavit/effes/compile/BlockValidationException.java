6ba368f2005e09fe2decc564703f1320c30beb12
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-25 12:54:25 -0600

    add block validation

---
+++ src/main/java/com/yuvalshavit/effes/compile/BlockValidationException.java
@@ -0,0 +1,7 @@
+package com.yuvalshavit.effes.compile;
+
+public final class BlockValidationException extends RuntimeException {
+  public BlockValidationException(String message) {
+    super(message);
+  }
+}
