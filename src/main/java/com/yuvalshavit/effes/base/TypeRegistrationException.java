29487fa00dc76ceb86e63393020b5e9044d8b905
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-25 01:29:15 -0600

    starting from the beginning, more or less

--- src/main/java/com/yuvalshavit/effes/base/TypeRegistrationException.java
+++
@@ -1,7 +0,0 @@
-package com.yuvalshavit.effes.base;
-
-public final class TypeRegistrationException extends RuntimeException {
-  public TypeRegistrationException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/yuvalshavit/effes/compile/TypeCompiler.java
+++
@@ -1,8 +0,0 @@
-package com.yuvalshavit.effes.compile;
-
-import com.yuvalshavit.effes.base.Type;
-import com.yuvalshavit.effes.parser.EffesParser;
-
-public interface TypeCompiler {
-  Type aooly(EffesParser.TypeContext ctx);
-}
