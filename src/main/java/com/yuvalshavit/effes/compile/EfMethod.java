bfcbad73b6d7a09623a87f9bb43a4086e1656451
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-25 12:33:57 -0600

    make EfMethod and MethodRegistry generic on body

--- src/main/java/com/yuvalshavit/effes/compile/EfMethod.java
+++ src/main/java/com/yuvalshavit/effes/compile/EfMethod.java
@@ -4,14 +4,21 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
+import java.util.function.Function;
 
-public final class EfMethod {
+public final class EfMethod<B> {
   private final List<SimpleType> argTypes;
   private final SimpleType resultType;
+  private final B body;
 
-  public EfMethod(List<SimpleType> argTypes, SimpleType resultType) {
+  public EfMethod(List<SimpleType> argTypes, SimpleType resultType, B body) {
     this.argTypes = ImmutableList.copyOf(argTypes);
     this.resultType = resultType;
+    this.body = body;
+  }
+
+  public <B2> EfMethod<B2> tranform(Function<B, B2> func) {
+    return new EfMethod<>(argTypes, resultType, func.apply(body));
   }
 
   @Override
