d42a57aeaaa47b7ba03c8c709ff02bcc140e45bf
Yuval Shavit <yankee.sierra@gmail.com>
2014-03-25 09:59:20 -0600

    add type registry, finder for data types

---
+++ src/main/java/com/yuvalshavit/effes/compile/SimpleType.java
@@ -0,0 +1,15 @@
+package com.yuvalshavit.effes.compile;
+
+public final class SimpleType {
+  private final String name;
+
+  public SimpleType(String name) {
+    assert name != null;
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
