120043363fd728358bc81ec260fd9a783901fd02
Yuval Shavit <yankee.sierra@gmail.com>
2014-04-02 20:11:58 -0600

    dd unused imports

--- src/main/java/com/yuvalshavit/effes/compile/TypeRegistry.java
+++ src/main/java/com/yuvalshavit/effes/compile/TypeRegistry.java
@@ -6,7 +6,6 @@ import org.antlr.v4.runtime.Token;
 import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
