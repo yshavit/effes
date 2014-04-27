ff8cdd27fd9d5d7fe2ff7f8b9932b220a61d20d0
Yuval Shavit <yankee.sierra@gmail.com>
2014-04-27 16:39:58 -0400

    add serialVersionUID where needed

--- src/main/java/com/yuvalshavit/effes/interpreter/BadMainException.java
+++ src/main/java/com/yuvalshavit/effes/interpreter/BadMainException.java
@@ -1,6 +1,8 @@
 package com.yuvalshavit.effes.interpreter;
 
 public class BadMainException extends RuntimeException {
+  private static final long serialVersionUID = -6027398474550520717L;
+
   BadMainException(String message) {
     super(message);
   }
--- src/main/java/com/yuvalshavit/effes/parser/AntlrParseException.java
+++ src/main/java/com/yuvalshavit/effes/parser/AntlrParseException.java
@@ -1,6 +1,9 @@
 package com.yuvalshavit.effes.parser;
 
 public class AntlrParseException extends RuntimeException {
+
+  private static final long serialVersionUID = -4308604862865727621L;
+
   public AntlrParseException(int line, int posInLine, String msg, Throwable cause) {
     // posInLine comes in 0-indexed, but we want to 1-index it so it lines up with what editors say (they
     // tend to 1-index)
