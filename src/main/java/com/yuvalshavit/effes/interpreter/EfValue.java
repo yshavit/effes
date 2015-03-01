3933f5025f09884d60e14d6efdc601b6a063f0aa
Yuval Shavit <yankee.sierra@gmail.com>
2015-02-28 20:23:45 -0700

    removing EfValue.EfStringValue

--- src/main/java/com/yuvalshavit/effes/interpreter/EfValue.java
+++ src/main/java/com/yuvalshavit/effes/interpreter/EfValue.java
@@ -13,9 +13,9 @@ public class EfValue {
   public static EfValue of(EfType.SimpleType type, List<EfValue> state) {
     return new EfValue(type, state);
   }
-
-  public static EfValue of(EfType.SimpleType type, String value) {
-    return new EfStringValue(type, value);
+  
+  public static EfValue of(String value) {
+    throw new UnsupportedOperationException(""); // TODO
   }
 
   public EfValue(EfType.SimpleType type, List<EfValue> state) {
@@ -58,41 +58,5 @@ public class EfValue {
     int result = type.hashCode();
     result = 31 * result + state.hashCode();
     return result;
-  }
-
-  public static class EfStringValue extends EfValue {
-    private final String value;
-
-    private EfStringValue(EfType.SimpleType type, String value) {
-      super(type, ImmutableList.of());
-      this.value = value;
-    }
-
-    public String getString() {
-      return value;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("%s(%s)", getType(), value.replace("(", "\\(").replace(")", "\\)"));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      if (!super.equals(o)) return false;
-
-      EfStringValue that = (EfStringValue) o;
-
-      return value.equals(that.value);
-    }
-
-    @Override
-    public int hashCode() {
-      int result = super.hashCode();
-      result = 31 * result + value.hashCode();
-      return result;
-    }
   }
 }
