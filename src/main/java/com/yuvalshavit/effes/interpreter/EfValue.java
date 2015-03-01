15221115fb83a675da74cae50afe49d9f10e6117
Yuval Shavit <yankee.sierra@gmail.com>
2015-02-28 20:53:55 -0700

    handle built-in types better
    ints should now be creatable, if not quite usable

--- src/main/java/com/yuvalshavit/effes/interpreter/EfValue.java
+++ src/main/java/com/yuvalshavit/effes/interpreter/EfValue.java
@@ -1,62 +1,156 @@
 package com.yuvalshavit.effes.interpreter;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.primitives.Longs;
+import com.yuvalshavit.effes.compile.node.BuiltinTypes;
 import com.yuvalshavit.effes.compile.node.EfType;
 
+import java.util.Collections;
 import java.util.List;
 
-public class EfValue {
+public abstract class EfValue {
   private final EfType.SimpleType type;
   private final List<EfValue> state;
 
   public static EfValue of(EfType.SimpleType type, List<EfValue> state) {
-    return new EfValue(type, state);
+    return new StandardValue(type, state);
   }
   
   public static EfValue of(String value) {
-    throw new UnsupportedOperationException(""); // TODO
+    return new StringValue(value);
+  }
+  
+  public static EfValue of(long value) {
+    return new LongValue(value);
   }
 
-  public EfValue(EfType.SimpleType type, List<EfValue> state) {
-    this.type = type;
-    // TODO also check types against expected
+  private EfValue(EfType.SimpleType type, List<EfValue> state) {
+    this.type = Preconditions.checkNotNull(type);
     this.state = ImmutableList.copyOf(state);
   }
 
   public EfType.SimpleType getType() {
     return type;
   }
-
+  
   public List<EfValue> getState() {
     return state;
   }
-
-  @Override
-  public String toString() {
-    if (state.isEmpty()) {
-      return type.getName();
+  
+  public static final class StandardValue extends EfValue {
+    
+    private StandardValue(EfType.SimpleType type, List<EfValue> state) {
+      super(type, state);
+      // TODO also check types against expected
     }
-    StringBuilder sb = new StringBuilder(type.getName()).append('(');
-    Joiner.on(", ").appendTo(sb, state);
-    sb.append(')');
-    return sb.toString();
+    
+    @Override
+    public String toString() {
+      if (getState().isEmpty()) {
+        return getType().getName();
+      }
+      StringBuilder sb = new StringBuilder(getType().getName()).append('(');
+      Joiner.on(", ").appendTo(sb, getState());
+      sb.append(')');
+      return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o)
+        return true;
+      if (o == null || o.getClass() != StandardValue.class)
+        return false;
+
+      StandardValue efValue = (StandardValue) o;
+      return getState().equals(efValue.getState()) && getType().equals(efValue.getType());
+
+    }
+
+    @Override
+    public int hashCode() {
+      int result = getType().hashCode();
+      result = 31 * result + getState().hashCode();
+      return result;
+    }
   }
+  
+  private static abstract class BuiltinValue<E extends BuiltinValue<E>> extends EfValue {
+    protected abstract boolean equalsComponent(E other);
+    @Override
+    public abstract int hashCode();
+    @Override
+    public abstract String toString();
+    
+    protected BuiltinValue(EfType.SimpleType type) {
+      super(type, Collections.emptyList());
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public final boolean equals(Object o) {
+      if (this == o)
+        return true;
+      if (o == null || getClass() != o.getClass())
+        return false;
 
-    EfValue efValue = (EfValue) o;
-    return state.equals(efValue.state) && type.equals(efValue.type);
+      @SuppressWarnings("unchecked")
+      E that = (E) o;
+      return equalsComponent(that);
 
+    }
   }
+  
+  public static final class LongValue extends BuiltinValue<LongValue> {
+    private final long value;
+    
+    private LongValue(long value) {
+      super(BuiltinTypes.efTypeFor(value));
+      this.value = value;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = type.hashCode();
-    result = 31 * result + state.hashCode();
-    return result;
+    @Override
+    public int hashCode() {
+      return Longs.hashCode(value);
+    }
+
+    @Override
+    protected boolean equalsComponent(LongValue other) {
+      return value == other.value;
+    }
+
+    @Override
+    public String toString() {
+      return String.valueOf(value);
+    }
+  }
+  
+  public static final class StringValue extends BuiltinValue<StringValue> {
+    private final String value;
+
+    private StringValue(String value) {
+      super(BuiltinTypes.STRING.getEfType());
+      this.value = Preconditions.checkNotNull(value);
+    }
+    
+    public String getString() {
+      return value;
+    }
+
+    @Override
+    public int hashCode() {
+      return value.hashCode();
+    }
+
+    @Override
+    protected boolean equalsComponent(StringValue other) {
+      return value.equals(other.value);
+    }
+
+    @Override
+    public String toString() {
+      return getString();
+    }
   }
 }
