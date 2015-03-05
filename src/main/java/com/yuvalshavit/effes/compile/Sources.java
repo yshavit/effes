8bb97e44c34984b557ab18c3fe8e5cf1afb4a8ad
Yuval Shavit <yankee.sierra@gmail.com>
2015-03-04 20:25:14 -0500

    create Builtin.ef

--- src/main/java/com/yuvalshavit/effes/compile/Sources.java
+++ src/main/java/com/yuvalshavit/effes/compile/Sources.java
@@ -1,12 +1,20 @@
 package com.yuvalshavit.effes.compile;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.function.Consumer;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Iterables;
 import com.yuvalshavit.effes.parser.EffesParser;
+import com.yuvalshavit.effes.parser.ParserUtils;
+import com.yuvalshavit.util.RelativeUrl;
 
 public class Sources {
   
@@ -35,8 +43,16 @@ public class Sources {
   }
 
   private static Iterable<EffesParser.CompilationUnitContext> defaultBuiltinSources() {
-    // TODO
-    return Collections.emptySet();
+    RelativeUrl relativeUrl = new RelativeUrl(Sources.class);
+    URL url = relativeUrl.get("Builtin.ef");
+    Collection<EffesParser.CompilationUnitContext> contexts = new ArrayList<>();
+    try (InputStream is = url.openStream(); InputStreamReader reader = new InputStreamReader(is, Charsets.UTF_8)) {
+      EffesParser parser = ParserUtils.createParser(reader);
+      contexts.add(parser.compilationUnit());
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    return contexts;
   }
 
 }
